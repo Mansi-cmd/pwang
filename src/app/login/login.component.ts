c9a03e6682a55f52b49252628902451e48ef834f
unknown <nayakmansi888@gmail.com>
2023-04-18 18:28:34 +0530

    fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  form = this.fb.group({
+    email:['',[Validators.email,Validators.required]],
+    password:['',[Validators.required]]
+});
+isSubmit = false;
+  constructor(
+    private fb: UntypedFormBuilder){}
 
+  onSubmit() {
+    this.isSubmit = true;
+    if(this.form.invalid) {
+      return
+    }
+
+    console.log(this.form.value);
+
+  }  
+
+  //getter functions  
+  get email():FormControl {
+    return this.form.get('email')  as FormControl;;
+  }
+  get password():FormControl {
+    return this.form.get('password')  as FormControl;;
+  }
 }
--- src/app/screens/dashboard/dashboard.component.ts
+++ src/app/screens/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  form = this.fb.group({
+    name:['',[Validators.required, Validators.pattern('[A-Za-z]*$')]],
+    experience:['',[Validators.email,Validators.required]],
+    education:['',[Validators.required,Validators.pattern('[A-Za-z]*$')]]
+  });
 
+  Submit = false;
+  constructor(
+    private fb: UntypedFormBuilder){}
+
+  onSubmit() {
+    this.Submit = true;
+    if(this.form.invalid) {
+      return
+    }
+
+    console.log(this.form.value);
+
+  }  
+
+  //getter functions  
+  get name():FormControl {
+    return this.form.get('name')  as FormControl;
+  }
+
+  get password():FormControl {
+    return this.form.get('experience')  as FormControl;
+  }
+
+  get email():FormControl {
+    return this.form.get('education')  as FormControl;
+  }
 }
